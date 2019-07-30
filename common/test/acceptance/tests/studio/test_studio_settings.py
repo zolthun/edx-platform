9349b5f8c1f16ee599278d933ecd0c85c886357f
jinder1s <msingh@edx.org>
2019-07-30 10:19:24 -0400

    pep8 fixes

--- common/test/acceptance/tests/studio/test_studio_settings.py
+++ common/test/acceptance/tests/studio/test_studio_settings.py
@@ -470,7 +470,7 @@ class AdvancedSettingsValidationTest(StudioCourseTest):
         self.advanced_settings.set('Discussion Topic Mapping', json_input)
         advanced_setting_get = self.advanced_settings.get('Discussion Topic Mapping')
         expected_output = u'{\n    "key": {\n        "id": "value_1"\n    },\n    '\
-                           '"key_2": {\n        "id": "value_2"\n    }\n}'
+                          '"key_2": {\n        "id": "value_2"\n    }\n}'
 
         self.assertEqual(advanced_setting_get, expected_output)
 
