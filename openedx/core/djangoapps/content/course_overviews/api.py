2eaa9beab50e7431be2c1e72dc7d5380ee15083c
Christopher Pappas <cpappas@edx.org>
2019-08-06 14:39:49 -0400

    CourseOverview serializer work

--- openedx/core/djangoapps/content/course_overviews/api.py
+++ openedx/core/djangoapps/content/course_overviews/api.py
@@ -4,17 +4,21 @@ from openedx.core.djangoapps.content.course_overviews.models import CourseOvervi
 
 from openedx.core.djangoapps.content.course_overviews.serializers import (
     CourseOverviewBaseSerializer,
-    CourseOverviewExtendedSerializer,
+    CourseOverviewRequestUserSerializer,
 )
 
 
-def get_course_overviews(course_ids, extra_fields=False):
+def get_course_overviews(course_ids, request=None):
     """
     Return course_ovewview data for a given list of course_ids.
     """
     overviews = CourseOverview.objects.filter(id__in=course_ids)
 
-    if extra_fields:
-        return CourseOverviewExtendedSerializer(overviews, many=True).data
+    if request:
+        return CourseOverviewRequestUserSerializer(
+            overviews,
+            many=True,
+            context={'request': request}
+        ).data
     return CourseOverviewBaseSerializer(overviews, many=True).data
 
--- openedx/core/djangoapps/content/course_overviews/serializers.py
+++ openedx/core/djangoapps/content/course_overviews/serializers.py
@@ -1,10 +1,21 @@
 from rest_framework import serializers
 
 from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
-
+from lms.djangoapps.certificates.api import get_certificate_for_user
+from lms.djangoapps.program_enrollments.api.api import (
+    get_due_dates,
+    get_course_run_url,
+    get_emails_enabled,
+    get_course_run_status
+)
 from lms.djangoapps.program_enrollments.api.v1.constants import (
     CourseRunProgressStatuses,
 )
+from opaque_keys.edx.locator import CourseLocator
+from student.helpers import get_resume_urls_for_enrollments
+from student.models import CourseEnrollment
+
+
 
 class CourseOverviewBaseSerializer(serializers.ModelSerializer):
     """
@@ -20,31 +31,62 @@ class CourseOverviewBaseSerializer(serializers.ModelSerializer):
         )
 
 
-class CourseOverviewExtendedSerializer(CourseOverviewBaseSerializer):
+class CourseOverviewRequestUserSerializer(CourseOverviewBaseSerializer):
     """
     A Serializer that adds a number of extra calculated fields for a given
     request user.
     """
 
-    STATUS_CHOICES = [
-        CourseRunProgressStatuses.IN_PROGRESS,
-        CourseRunProgressStatuses.UPCOMING,
-        CourseRunProgressStatuses.COMPLETED
-    ]
-
     def to_representation(self, instance):
-        representation = super(CourseOverviewExtendedSerializer, self).to_representation(instance)
+        representation = super(CourseOverviewRequestUserSerializer, self).to_representation(instance)
 
-        # Here is where we will add a bunch of the extra logic from program
-        # enrollments endpoint that we're interested in
-        representation['course_run_status'] = '______'
-        representation['display_name'] = '______'
-        representation['resume_course_run_url'] = '______'
-        representation['course_run_url'] = '______'
-        representation['emails_enabled'] = '______'
-        representation['due_dates'] = '______'
-        representation['micromasters_title'] = '______'
-        representation['certificate_download_url'] = '______'
+        request = self.context['request']
+
+        # Following fields optionally added to representation
+        # Certificate
+        certificate_download_url = None
+        is_certificate_passing = None
+        certificate_creation_date = None
+        certificate_info = get_certificate_for_user(
+            request.user.username,
+            instance.id
+        )
+        if certificate_info:
+            certificate_download_url = certificate_info['download_url']
+            is_certificate_passing = certificate_info['is_passing']
+            certificate_creation_date = certificate_info['created']
+        if certificate_download_url:
+            representation['certificate_download_url'] = certificate_download_url
+
+        # Resume url
+        enrollment = CourseEnrollment.objects.get(
+            user__username=request.user.username,
+            course_id=instance.id,
+        )
+        resume_course_run_url = get_resume_urls_for_enrollments(
+            request.user,
+            [enrollment],
+        )[instance.id]
+        if resume_course_run_url:
+            representation['resume_course_run_url'] = resume_course_run_url
+
+        # Email enabled
+        emails_enabled = get_emails_enabled(request.user, instance.id)
+        if emails_enabled is not None:
+            representation['emails_enabled'] = emails_enabled
+
+        # From here, these fields are always added
+        representation['course_run_id'] = str(instance.id),
+        representation['course_run_status'] = get_course_run_status(
+            instance,
+            is_certificate_passing,
+            certificate_creation_date,
+        )
+        representation['start_date'] = instance.start,
+        representation['end_date'] = instance.end,
+        representation['display_name'] = instance.display_name_with_default
+        representation['course_run_url'] = get_course_run_url(request, instance.id)
+        representation['due_dates'] = get_due_dates(request, instance.id, request.user)
 
         return representation
 
