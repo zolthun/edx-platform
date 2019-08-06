a9453c8a5ad306e3d37bb8512a4365bef6a90fd7
Christopher Pappas <cpappas@edx.org>
2019-08-06 14:45:19 -0400

    Removing enterprise learner portal djangoapp from this repo

--- lms/djangoapps/enterprise_learner_portal/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class EnterpriseLearnerPortalConfig(AppConfig):
-    """
-    Configuration for the enterprise Django application.
-    """
-
-    name = "enterprise_learner_portal"
