d41a66866337939b8736b45cc9f01ed8270a5e1e
Christopher Pappas <cpappas@edx.org>
2019-08-07 17:12:42 -0400

    Updating serializers again

--- openedx/core/djangoapps/content/course_overviews/api.py
+++ openedx/core/djangoapps/content/course_overviews/api.py
@@ -6,21 +6,13 @@ from openedx.core.djangoapps.content.course_overviews.models import CourseOvervi
 
 from openedx.core.djangoapps.content.course_overviews.serializers import (
     CourseOverviewBaseSerializer,
-    CourseOverviewRequestUserSerializer,
 )
 
 
-def get_course_overviews(course_ids, request=None):
+def get_course_overviews(course_ids):
     """
     Return course_ovewview data for a given list of course_ids.
     """
     overviews = CourseOverview.objects.filter(id__in=course_ids)
-
-    if request:
-        return CourseOverviewRequestUserSerializer(
-            overviews,
-            many=True,
-            context={'request': request}
-        ).data
     return CourseOverviewBaseSerializer(overviews, many=True).data
 
--- openedx/core/djangoapps/content/course_overviews/serializers.py
+++ openedx/core/djangoapps/content/course_overviews/serializers.py
@@ -4,16 +4,7 @@ CourseOverview serializers
 """
 from rest_framework import serializers
 
-from lms.djangoapps.certificates.api import get_certificate_for_user
-from lms.djangoapps.program_enrollments.api.api import (
-    get_due_dates,
-    get_course_run_url,
-    get_emails_enabled,
-    get_course_run_status
-)
 from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
-from student.helpers import get_resume_urls_for_enrollments
-from student.models import CourseEnrollment
 
 
 class CourseOverviewBaseSerializer(serializers.ModelSerializer):
@@ -23,69 +14,12 @@ class CourseOverviewBaseSerializer(serializers.ModelSerializer):
 
     class Meta(object):
         model = CourseOverview
-        fields = (
-            'id',
-            'start',
-            'end',
-        )
-
-
-class CourseOverviewRequestUserSerializer(CourseOverviewBaseSerializer):
-    """
-    A Serializer that adds a number of extra calculated fields for a given
-    user included in the request context.
-    """
+        fields = '__all__'
 
     def to_representation(self, instance):
-        representation = super(CourseOverviewRequestUserSerializer, self).to_representation(instance)
-
-        request = self.context['request']
-
-        # Following fields optionally added to representation
-        # Certificate
-        certificate_download_url = None
-        is_certificate_passing = None
-        certificate_creation_date = None
-        certificate_info = get_certificate_for_user(
-            request.user.username,
-            instance.id
-        )
-        if certificate_info:
-            certificate_download_url = certificate_info['download_url']
-            is_certificate_passing = certificate_info['is_passing']
-            certificate_creation_date = certificate_info['created']
-        if certificate_download_url:
-            representation['certificate_download_url'] = certificate_download_url
-
-        # Resume url
-        enrollment = CourseEnrollment.objects.get(
-            user__username=request.user.username,
-            course_id=instance.id,
-        )
-        resume_course_run_url = get_resume_urls_for_enrollments(
-            request.user,
-            [enrollment],
-        )[instance.id]
-        if resume_course_run_url:
-            representation['resume_course_run_url'] = resume_course_run_url
-
-        # Email enabled
-        emails_enabled = get_emails_enabled(request.user, instance.id)
-        if emails_enabled is not None:
-            representation['emails_enabled'] = emails_enabled
-
-        # Following fields always added to the representation
-        representation['course_run_id'] = str(instance.id),
-        representation['course_run_status'] = get_course_run_status(
-            instance,
-            is_certificate_passing,
-            certificate_creation_date,
-        )
-        representation['start_date'] = instance.start,
-        representation['end_date'] = instance.end,
-        representation['display_name'] = instance.display_name_with_default
-        representation['course_run_url'] = get_course_run_url(request, instance.id)
-        representation['due_dates'] = get_due_dates(request, instance.id, request.user)
-
+        representation = super(CourseOverviewBaseSerializer, self).to_representation(instance)
+        representation['display_name_with_default'] = instance.display_name_with_default
+        representation['has_started'] = instance.has_started
+        representation['has_ended'] = instance.has_ended
+        representation['pacing'] = instance.pacing
         return representation
-
